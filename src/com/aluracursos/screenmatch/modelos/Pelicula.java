93c1913c174a46cc7548f4c4a103ee1b09164b8c
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-04-13 20:46:19 -0300

    Update ap

--- src/com/aluracursos/screenmatch/modelos/Pelicula.java
+++ src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -1,63 +1,11 @@
 package com.aluracursos.screenmatch.modelos;
 
-public class Pelicula {
-    private String nombre;
-    private int fechaLanzamiento;
-    private int duracionMinutos;
+public class Pelicula extends Titulo {
 
-    private boolean incluidoEnPlan;
-    private double sumaEvaluaciones;
+    private String director;
 
-    private int contador;
-
-
-    //setters
-
-    public void setNombre(String nombre){
-        this.nombre = nombre;
+    public String getDirector(){
+        return getDirector();
     }
-
-    public void setFechaLanzamiento (int fechaLanzamiento){
-        this.fechaLanzamiento = fechaLanzamiento;
-    }
-
-    public void setDuracionMinutos (int duracionMinutos){
-        this.duracionMinutos = duracionMinutos;
-    }
-
-    public void setIncluidoEnPlan (boolean incluidoEnPlan){
-        this.incluidoEnPlan = incluidoEnPlan;
-    }
-
-    //get
-    public int getContador(){
-        return contador;
-    }
-    public String getNombre(){
-        return nombre;
-    }
-
-/************************ Otros métodos *************************************************************/
-    public void evalua (double nota){
-        sumaEvaluaciones += nota;
-        contador++;
-
-    }
-    double promedioEvaluaciones(){
-        return Math.round((sumaEvaluaciones/contador) * 10.0) / 10.0;
-
-
-    }
-
-    public void muestraFichaTecnica(){
-        System.out.println("Nombre de la película = " + nombre);
-        System.out.println("Fecha de lanzamiento = " + fechaLanzamiento);
-        System.out.println("Duración = " + duracionMinutos + " minutos");
-        String mensajePlan = incluidoEnPlan? "Incluida en su plan" : "No disponible en su plan" ;
-        System.out.println(mensajePlan);
-        System.out.println("Calificación = " + promedioEvaluaciones() + " / 5");
-        System.out.println("\n");
-    }
-
 
 }
---
+++ src/com/aluracursos/screenmatch/modelos/Serie.java
@@ -0,0 +1,20 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Serie extends Titulo{
+
+    private int temporadas;
+    private int episodiosPorTemporadas;
+    private int minutosPorEpisodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public int getEpisodiosPorTemporadas() {
+        return episodiosPorTemporadas;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+}

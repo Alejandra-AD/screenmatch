79be17c94d52336a9936ad00d73bf916a3dca873
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-04-15 15:43:39 -0300

    Update app

--- src/com/aluracursos/screenmatch/modelos/Titulo.java
+++ src/com/aluracursos/screenmatch/modelos/Titulo.java
@@ -56,7 +56,7 @@ public class Titulo {
     public void muestraFichaTecnica(){
         System.out.println("Nombre de la película = " + nombre);
         System.out.println("Fecha de lanzamiento = " + fechaLanzamiento);
-        System.out.println("Duración = " + duracionMinutos + " minutos");
+        System.out.println("Duración = " + getDuracionMinutos()+ " minutos");
         String mensajePlan = incluidoEnPlan? "Incluida en su plan" : "No disponible en su plan" ;
         System.out.println(mensajePlan);
         System.out.println("Calificación = " + promedioEvaluaciones() + " / 5");
